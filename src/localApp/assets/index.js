57a6f8a2fde35a9137030a931771128273dd25df
concefly <98969118coding>
2017-02-04 16:18:34 +0800

    qrcode

--- src/localApp/assets/index.js
+++ src/localApp/assets/index.js
@@ -5,6 +5,11 @@ function updateAll() {
   $.getJSON('/sysInfo', function(data) {
     $('#uptime').text(Math.floor(data.uptime / 3600) + ' 小时');
     $('#ip').text(data.addrList[0]);
+    // qrcode
+    var qr = qrcode(4, 'L');
+    qr.addData('http://'+data.addrList[0]+':3000');
+    qr.make();
+    document.getElementById('qrcode').innerHTML = qr.createImgTag(3,0);
   });
   $('#time').text(moment().format('YYYY-M-D k:m'));
 }
